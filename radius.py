d6067aa648f6265349ce48f3b5950ecadc0e9653
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-08-30 22:01:04 +0530

    Find the value of radius of circle

---
+++ radius.py
@@ -0,0 +1,12 @@
+#Find the value of radius of circle whose area is equal to the area of rectangle whose both sides are given
+
+l = 10
+b = 5
+
+#calculating the area of rectangle
+area = l * b
+
+#calculating the radius 
+r = ( (area)/3.142 ) ** 0.5
+
+print('radius = ',r)
