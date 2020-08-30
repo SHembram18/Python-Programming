4e0f5f0273e9499f43e24468d071a2387e85ca56
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-08-30 22:18:39 +0530

    Swapping two number without using third variable

---
+++ Swap.py
@@ -0,0 +1,16 @@
+
+#PROGRAM :- Swapping two number without using third variable
+#FILE    :- Swap.py
+#CREATED BY :- Sant0sh Hembram
+#DATED     :- 29/08/20
+
+A   =   100
+B   =   200
+
+print('Before swapping value : ' 'A = ',A , ',' 'B = ',B)
+
+A   =   A   +   B
+B   =   A   -   B
+A   =   A   -   B
+
+print('After swapping value : ' 'A = ',A , ',' 'B = ',B)
