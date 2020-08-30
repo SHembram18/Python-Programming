38e70b9a2b99d98ab4c3ee67174ee5c4ee2cb1c4
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-08-30 21:59:30 +0530

    Convert seconds into corresponding hour, minute

---
+++ covert_time.py
@@ -0,0 +1,9 @@
+#Convert seconds into corresponding hour, minute
+
+seconds = 3600
+print('seconds = ',seconds)
+hour =  seconds // 3600
+minute = seconds // 60
+
+print('After converting seconds into hour = ',hour ,'h')
+print('After converting seconds into minute = ',minute ,'min')
