3e8655a7f867dd2a5dc77316db6c9ff7535ae35a
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-09-08 04:52:45 +0530

    Check whether a given year is leap year or not

---
+++ Assignment-2/LeapYear.py
@@ -0,0 +1,18 @@
+# Check whether a given year is leap year or not
+
+year = int(input("Enter a year: "))
+
+if(year % 4) == 0:
+    if(year % 100) == 0:
+        if(year % 400) == 0:
+            print(year," is a leap year")
+
+        else:
+            print("{0} is not a leap year".format(year))
+
+    else:
+        print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
+
+    
