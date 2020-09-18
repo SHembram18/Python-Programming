aae42f156781dbba07fd92b8aee245b63867ed62
Santosh Hembram <santoshhembramis17489@gmail.com>
2020-09-18 20:41:45 +0530

    Add files via upload

---
+++ MyWorks/printDemo1.py
@@ -0,0 +1,7 @@
+# Program: Exploring the print()
+# File: printDemo1.py
+
+x = int(input("Please enter an integer: "))
+print("X = " +str(x))
+print("X = {0}".format(x))
+print("%d" % ( int(x)) )
---
+++ MyWorks/printDemo2.py
@@ -0,0 +1,20 @@
+# Program: Printing style of output
+# File: printDemo2.py
+
+print(3,4,5,6, sep='*')
+
+print(3,4,5,6)
+
+print("3 4 5 6")
+
+print(3,4,5,6, sep=',')
+
+print(3,21.45,678, "IMCA")
+
+print(15.00, 34, 95, "IMCA", sep='~')
+
+print(3,4,5,6, sep=',', end=' & ')
+
+print(34.56, 546, "imca", sep=":")
+
+
